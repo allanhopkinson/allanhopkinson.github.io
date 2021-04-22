a97806f586faf995944baab12629084f228687a2
allanhopkinson <allanghopkinson@gmail.com>
2021-04-21 21:59:56 -0600

    css and content update

--- Javascript/theDate.js
+++ Javascript/theDate.js
@@ -1,2 +1,4 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('the_date').textContent = new Date().toLocaleDateString('en-US', options);
+const options = {year: 'numeric'};
+document.querySelector('#the_year').textContent = new Date().toLocaleDateString('en-US', options);
+let upDate = document.lastModified;
+document.getElementById('update').textContent = upDate;
