c661d1de1a8e76aba97846e5d1052a6f39edc722
allanhopkinson <allanghopkinson@gmail.com>
2021-04-22 18:51:35 -0600

    Initial Commit

--- Javascript/theDate.js
+++
@@ -1,4 +0,0 @@
-const options = {year: 'numeric'};
-document.querySelector('#the_year').textContent = new Date().toLocaleDateString('en-US', options);
-let upDate = document.lastModified;
-document.getElementById('update').textContent = upDate;
---
+++ js/main.js
@@ -0,0 +1,2 @@
+document.querySelector('#the_year').textContent = new Date().getFullYear();
+document.getElementById('update').textContent = document.lastModified;
